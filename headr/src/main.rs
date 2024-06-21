d4cfd157fd5133a9b4229e51588798cdafb797f5
s0rbus <soorbus@gmail.com>
2024-06-21 09:44:21 +0100

    ch4 opening files

--- headr/src/main.rs
+++ headr/src/main.rs
@@ -1,3 +1,9 @@
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
+
+use anyhow::Result;
 use clap::Parser;
 
 ///Rust version of 'head'
@@ -11,18 +17,40 @@ struct Args {
     ///Number of lines to print
     #[arg(
         short('n'),
-        long("lines"),
+        long,
+        value_name = "LINES",
         conflicts_with("bytes"),
-        default_value_t = 10
+        default_value_t = 10,
+        value_parser = clap::value_parser!(u64).range(1..),
     )]
     lines: u64,
 
     ///Number of bytes to print
-    #[arg(short('c'), long("bytes"))]
+    #[arg(short('c'), value_name = "BYTES", long, value_parser = clap::value_parser!(u64).range(1..),)]
     bytes: Option<u64>,
 }
 
 fn main() {
-    let args = Args::parse();
-    println!("{:#?}", args);
+    if let Err(e) = run(Args::parse()) {
+        eprintln!("{e}");
+        std::process::exit(1);
+    }
+    //println!("{:#?}", args);
+}
+
+fn run(args: Args) -> Result<()> {
+    for filename in args.files {
+        match open(&filename) {
+            Err(err) => eprintln!("{filename}: {err}"),
+            Ok(_) => println!("opened {filename}"),
+        }
+    }
+    Ok(())
+}
+
+fn open(filename: &str) -> Result<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
